6b1bdf9ab8cbaa91360fe16a8c7cca1aa1db04da
LakshmiReddy9988 <reddyb9052@gmail.com>
2024-02-23 08:53:51 +0530

    Ways to create and call Functional Components

--- my-app/src/components/functional-components.js
+++ my-app/src/components/functional-components.js
@@ -20,3 +20,29 @@ function show() {
     return <h1>Header</h1>
 }
 export default Header
+
+/**
+ * Functional components can be created in different ways
+    1. Using Named Function
+        function App() {
+            return (
+                <h1>Named Function</h1>
+            )
+        }
+    2. Using Arrow Function and it do not need any return statements as JSX expresssions are considered as single statements
+       let App = () => 
+        <div>
+            <h1>Arrow Function</h1>
+        </div>
+    3. Using Anonymous Function
+        let App = function () {
+            return (
+                <h1>Anonymous Function</h1>
+            )
+        }
+    
+ * Functional components can be called in different ways
+    1. App()
+    2. <App /> =>These are auto complete tags that doesn't contain any data
+    3. <App>Props Children data</App> =>Props children data is the data between the tags 
+ */
