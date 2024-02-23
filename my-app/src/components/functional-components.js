50b6c9e2cf2d4212a4054955c76b587a6ed4b4e5
LakshmiReddy9988 <reddyb9052@gmail.com>
2024-02-23 07:19:23 +0530

    JSX Attributes & Expressions

--- my-app/src/components/functional-components.js
+++ my-app/src/components/functional-components.js
@@ -1,11 +1,19 @@
-function Header() {
+import '../index.css';
+export function Header() {
     let menus = ['home', 'About Us', 'Contact US'];
     return (<header>
+        {show()}
         <ul>
-            <li>{menus[0]}</li>
+            <li className='test'>{menus[0]}</li>
             <li>{menus[1]}</li>
             <li>{menus[2]}</li>
         </ul>
+        <input type='checkbox' id='sub' />
+        <label htmlFor='sub'>Subscribe</label>
     </header>)
+}
+
+function show() {
+    return <h1>Header</h1>
 }
 export default Header
