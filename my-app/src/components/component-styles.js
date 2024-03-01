1c570a8d01009fe7945f89940d9bd51abb2dd211
LakshmiReddy9988 <reddyb9052@gmail.com>
2024-03-01 11:42:57 +0530

    Two way data binding in React

--- my-app/src/components/component-styles.js
+++ my-app/src/components/component-styles.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import './component-styles.css';
 export default function Styles() {
     let pStyles = {
         border: '2px solid red',
         marginTop: '10px'
     }
+
+    let [num, updateNum] = useState(100);
+
     return (
         <>
         <h1 style={{color: 'red'}} id='header'>This is heading</h1>
         <p style={pStyles}>This is Paragrapah</p>
+        <div>{num}</div>
+        <input type='text' value={num} onChange={(e) => {
+            let val = e.target.value;
+            updateNum(val);
+        }} />
         </>
     )
 }
